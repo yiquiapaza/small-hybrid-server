1418af6e080d89a7bacad168e71f4d1eabe34323
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-07-26 14:50:35 -0300

    Update position route

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
+var cors = require('cors');
 const router = express.Router();
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/dist')));
 
-var position = { position_x: 0.0, position_y: 0.0 };
+let position = { position_x: 0.0, position_y: 0.0 };
+let state = { state: 0 };
 
-router.get('/', (req, res) => {
+router.get('/', (_, res) => {
+	res.sendFile('index.html', { root: path.join(__dirname, '/dist') });
+});
+
+router.get('/position', (_, res) => {
 	res.status(200).send(position);
 });
 
-router.post('/', (req, res) => {
+router.post('/position', (req, res) => {
 	position = req.body;
-	res.status(200).end('yes');
+	res.status(200).end('Success');
 });
 
 app.use(router);
